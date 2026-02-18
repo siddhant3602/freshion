5822959a1bc4c35b8cc3b85fa02c9457f56761f4
Siddhant Singh <siddhantsingh2436@gmail.com>
2026-02-18 13:07:02 +0530

    fix case-sensitive import for OurPolicy

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
-import OurPolicy from '../components/ourPolicy'
+import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
 
 const Home = () => {
